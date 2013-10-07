7f992ff922374c7bd778050b69083d0d0f868bca
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2013-10-07 21:34:16 +0200

    bisschen style

--- app/forms.py
+++ app/forms.py
@@ -6,11 +6,11 @@ from wtforms import TextField, PasswordField, SubmitField, HiddenField, TextArea
 from config import ALLOWED_EXTENSIONS
 
 class LoginForm(Form):
-    username = TextField('Username', validators=[validators.Length(min=2, message=u'Mindestens zwei Zeichen benötigt'), validators.Required(message='Der Name muss dabei sein.')], description='LDAP Login Name')
-    password = PasswordField('Password', validators=[validators.Required(message='Ohne Password kein Login')], description='Passwort')
+    username = TextField('Username', validators=[validators.Length(min=2, message=u'Zu wenig Text..'), validators.Required(message='Der Name muss dabei sein.')], description='LDAP Login Name')
+    password = PasswordField('Passwort', validators=[validators.Required(message='Ohne Password kein Login. So einfach ist das.')], description='Passwort')
     submit = SubmitField('Login')
 
 class TaskForm(Form):
-    description = TextAreaField('Beschreibung', validators=[validators.Length(min=3, message='Wenigstens drei Zeichen solltest du schon schreiben..')], description='Heute habe ich...')
-    image = FileField('image', validators=[FileAllowed(ALLOWED_EXTENSIONS, 'Nur Bilder sind erlaubt (%s)!!1!' %(ALLOWED_EXTENSIONS))])
+    description = TextAreaField('Beschreibung', validators=[validators.Length(min=3, message='Zu wenig Text..')], description='Heute habe ich...')
+    image = FileField('Bild', validators=[FileAllowed(ALLOWED_EXTENSIONS, 'Nur Bilder sind erlaubt (%s)!!1!' %(', '.join(ALLOWED_EXTENSIONS)))])
     submit = SubmitField('Los')
--- app/store.py
+++ app/store.py
@@ -1,6 +1,4 @@
 # -.- coding: UTF-8 -.-
 
 def store(description, image):
-    print '\n\n\n'
     print '>', description, '|', image, '<'
-    print '\n\n\n'
