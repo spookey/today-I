b56e79205b0fde80a5f9c9a9d319038711877031
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2013-11-27 15:22:39 +0100

    Repo nach Github ziehen, die Jungs sind zu faul den Server wieder herzustellen..

---
+++ Readme.markdown
@@ -0,0 +1,9 @@
+# Today I
+
+We had the urge to broadcast our activities (things we did today) from our local Hackerspace to the world.
+
+This is a very quick and dirty hack, using our LDAP Server for authentication and our Wordpress Blog as target Platform, but it works and this is what matters!
+
+:octocat:
+
+
---
+++ config.py
@@ -0,0 +1,47 @@
+# -.- coding: UTF-8 -.-
+
+from os import path
+
+basedir = path.abspath(path.dirname(__file__))
+
+# logging
+logdir = path.join(basedir, 'logs')
+logfile = path.join(logdir, 'logfile.log')
+
+# tasks
+taskdir = path.join(basedir, 'tasks')
+taskjson = path.join(taskdir, 'current.json')
+taskattachdir = path.join(taskdir, 'images')
+
+# task archives (in subfolders by date)
+taskarchive = path.join(taskdir, 'archive')
+taskarchivejson_name = 'current.json'
+
+# report counter
+reportcounterjson = path.join(taskdir, 'reportcounter.json')
+
+# allowed extensions for upload
+ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
+
+#>>> import os
+#>>> os.urandom(24)
+SECRET_KEY = 'Set your secret key for flask here!'
+
+# LDAP login
+LDAPserver = 'ldap.awesome-server.com'
+LDAPPort = 636
+LDAPbasedn = 'ou=users,dc=awesome,dc=de'
+LDAPinactivegid = 1337
+
+# Wordpress login
+WPxmlrpc = 'http://your.awesome-blog.com/xmlrpc.php'
+WPuser = 'user'
+WPpass = 'pass'
+
+# Wordpress Post Template
+pskel = u'<li>{user}:<br />\n{image}<i>{description}</i></li>\n'
+iskel = u'<img src="{imageurl}" alt="{imagealt}"><br />\n'
+
+report_headline = u'Weekly Report'
+post_tag = ['awesome']
+category = ['Awesome Posts']
